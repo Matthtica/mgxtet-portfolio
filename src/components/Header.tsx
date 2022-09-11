bd0d6ad5718cc63bd56daef324550944fff873a5
Matthtica <zxnightstalker199733@gmail.com>
2022-09-11 19:43:05 +0630

    head added

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import styles from './Header.module.scss'
+
+export default function Header() {
+    return (
+        <div>
+            <div></div>
+            <h1>Skill</h1>
+        </div>
+    )
+}
